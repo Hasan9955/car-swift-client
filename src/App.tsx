3a6cc5d7c9bdcb2903d06219934deeaef30e8db6
Hasan9955 <hasanbinali7556@gmail.com>
2024-10-04 11:14:08 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+ 
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+    <h1>Hasan Bin Ali</h1>
+      <button className='btn btn-primary'>Click Me</button>
+    </>
+  )
+}
+
+export default App
